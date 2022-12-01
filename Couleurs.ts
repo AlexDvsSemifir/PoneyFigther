30c1db861f0ee8b95f1c98f5ce05714fa198602a
AlexDvsSemifir <alexandre.devos@semifir.com>
2022-12-01 09:46:13 +0100

    feat: ajout de l'enum des couleurs :sparkles:

---
+++ Couleurs.ts
@@ -0,0 +1,14 @@
+enum Couleurs {
+    ROUGE = "rouge",
+    BLEU = "bleu",
+    VERT = "vert",
+    JAUNE = "jaune",
+    ORANGE = "orange",
+    VIOLET = "violet",
+    BLANC = "blanc",
+    NOIR = "noir",
+    GRIS = "gris"
+}
+
+export default Couleurs;
+
